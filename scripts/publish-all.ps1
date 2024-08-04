f735ffab5166f851ef855373a25fea8a1578c0cc
Dr.Abc <me@drabc.net>
2024-08-04 23:03:38 +0800

    1

--- scripts/publish-all.ps1
+++ scripts/publish-all.ps1
@@ -32,10 +32,10 @@ switch ($os)
         $ProgramFiles = [Environment]::GetEnvironmentVariable("ProgramFiles(x86)")
         $vsLocation= &@("$ProgramFiles\Microsoft Visual Studio\Installer\vswhere.exe") "-latest" "-products" "*" "-requires" "Microsoft.VisualStudio.Component.VC.Tools.x86.x64" "-property" "installationPath"
 
+        &"nuget" "restore"
         $sharpproj = "SPRView.Net.Win32.Thumbnail"
         foreach($proj in $sharpproj){
             Set-Location $proj
-            &"nuget" "restore"
             if(Test-Path("$($vsLocation)\Common7\Tools\vsdevcmd.bat")){
                 &"$($vsLocation)\Common7\Tools\vsdevcmd.bat" "-arch=x64"
                 &"$($vsLocation)\Msbuild\Current\Bin\MSBuild.exe" "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)/../SPRView.Net.Win32.Thumbnail/SPRView.Net.Win32.Thumbnail.vcxproj" /p:Configuration="Release" /p:Platform="x64"
