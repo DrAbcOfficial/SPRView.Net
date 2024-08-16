999c18e5b6671cd131d9c24458f5fbd296f53ac7
Dr.Abc <me@drabc.net>
2024-08-16 17:41:23 +0800

    domo wtf = san

--- SPRView.Net.Win32.Thumbnail/RecipeThumbnailProvider.cpp
+++ SPRView.Net.Win32.Thumbnail/RecipeThumbnailProvider.cpp
@@ -32,6 +32,9 @@ WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
 #include "pch.h"
 #include "RecipeThumbnailProvider.h"
 #include <Shlwapi.h>
+#include <vector>
+#include <string>
+#include <sstream>
 
 #pragma comment(lib, "Shlwapi.lib")
 
@@ -117,106 +120,82 @@ IFACEMETHODIMP RecipeThumbnailProvider::Initialize(IStream* pStream, DWORD grfMo
 // valid alpha information.
 IFACEMETHODIMP RecipeThumbnailProvider::GetThumbnail(UINT cx, HBITMAP* phbmp, WTS_ALPHATYPE* pdwAlpha)
 {
-    BITMAPINFO bmi = { sizeof(bmi.bmiHeader) };
-    bmi.bmiHeader.biWidth = 32;
-    bmi.bmiHeader.biHeight = -static_cast<LONG>(32);
+    STATSTG statstg;
+    if (FAILED(m_pStream->Stat(&statstg, STATFLAG_NONAME)))
+        return S_FALSE;
+    ULARGE_INTEGER uliSize = statstg.cbSize;
+    std::vector<byte> buffer(static_cast<size_t>(uliSize.QuadPart));
+    ULONG bytesRead;
+    if (FAILED(m_pStream->Read(buffer.data(), static_cast<ULONG>(buffer.size()), &bytesRead)))
+        return S_FALSE;
+    m_pStream->Release();
+    m_pStream = NULL;
+    //fuck, not spr
+    if (buffer.size() < 34)
+        return S_FALSE;
+    std::istringstream input(std::string(buffer.begin(), buffer.end()), std::ios::binary);
+    int tempint = 0;
+    input.read(reinterpret_cast<char*>(&tempint), sizeof(tempint));
+    if (tempint != 0x50534449)
+        return S_FALSE;
+    input.read(reinterpret_cast<char*>(&tempint), sizeof(tempint));
+    if (tempint != 0x00000002)
+        return S_FALSE;
+    input.seekg(32, std::ios::cur);
+    short palettesize = 0;
+    input.read(reinterpret_cast<char*>(&palettesize), sizeof(palettesize));
+    using Color = struct
+    {
+        byte R;
+        byte G;
+        byte B;
+    };
+    std::vector<Color*> colors = {};
+    for (short i = 0; i < palettesize; i++) {
+        Color* c = new Color();
+        input.read(reinterpret_cast<char*>(c), sizeof(Color));
+    }
+    std::streampos currentPos = input.tellg();
+    input.seekg(12, std::ios::cur);
+    //width
+    int width = 0;
+    input.read(reinterpret_cast<char*>(&width), sizeof(width));
+    //height
+    int height = 0;
+    input.read(reinterpret_cast<char*>(&height), sizeof(height));  
+    BITMAPINFO bmi;
+    ZeroMemory(&bmi, sizeof(BITMAPINFO));
+    bmi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+    bmi.bmiHeader.biWidth = width;
+    bmi.bmiHeader.biHeight = -height;
     bmi.bmiHeader.biPlanes = 1;
-    bmi.bmiHeader.biBitCount = 32;
+    bmi.bmiHeader.biBitCount = 24;
     bmi.bmiHeader.biCompression = BI_RGB;
-    BYTE* pBits;
-    HBITMAP hBitmap = CreateDIBSection(NULL, &bmi, DIB_RGB_COLORS,
-        reinterpret_cast<void**>(&pBits), NULL, 0);
+    HDC hdc = GetDC(NULL);
+    void* pBits = NULL;
+    HBITMAP hBitmap = CreateDIBSection(hdc, &bmi, DIB_RGB_COLORS, &pBits, NULL, 0);
     if (hBitmap && pBits) {
+        int rowBytes = ((width * 3 + 3) & ~3);
         BYTE* pRow = (BYTE*)pBits;
-        for (int y = 0; y < 32; ++y) {
-            for (int x = 0; x < 32; ++x) {
-                pRow[x * 3 + 0] = 255;
-                pRow[x * 3 + 1] = 0;
-                pRow[x * 3 + 2] = 0;
-                pRow[x * 3 + 3] = 255;
-                pRow += 4;
+        for (int y = 0; y < height; ++y) {
+            for (int x = 0; x < width; ++x) {
+                byte data = 0;
+                input.read(reinterpret_cast<char*>(&data), sizeof(data));
+                Color* color = colors[data];
+                pRow[x * 3 + 0] = color->B;
+                pRow[x * 3 + 1] = color->G;
+                pRow[x * 3 + 2] = color->R;
             }
+            pRow += rowBytes;
         }
     }
+    ReleaseDC(NULL, hdc);
     *phbmp = hBitmap;
-    *pdwAlpha = WTS_ALPHATYPE::WTSAT_ARGB;
-
-    //STATSTG statstg;
-    //if (FAILED(m_pStream->Stat(&statstg, STATFLAG_NONAME)))
-    //    return S_FALSE;
-    //ULARGE_INTEGER uliSize = statstg.cbSize;
-    //std::vector<byte> buffer(static_cast<size_t>(uliSize.QuadPart));
-    //ULONG bytesRead;
-    //if (FAILED(m_pStream->Read(buffer.data(), static_cast<ULONG>(buffer.size()), &bytesRead)))
-    //    return S_FALSE;
-    //m_pStream->Release();
-    //m_pStream = NULL;
-    ////fuck, not spr
-    //if (buffer.size() < 34)
-    //    return S_FALSE;
-    //std::istringstream input(std::string(buffer.begin(), buffer.end()), std::ios::binary);
-    //int tempint = 0;
-    //input.read(reinterpret_cast<char*>(&tempint), sizeof(tempint));
-    //if (tempint != 0x50534449)
-    //    return S_FALSE;
-    //input.read(reinterpret_cast<char*>(&tempint), sizeof(tempint));
-    //if (tempint != 0x00000002)
-    //    return S_FALSE;
-    //input.seekg(32, std::ios::cur);
-    //short palettesize = 0;
-    //input.read(reinterpret_cast<char*>(&palettesize), sizeof(palettesize));
-    //using Color = struct
-    //{
-    //    byte R;
-    //    byte G;
-    //    byte B;
-    //};
-    //std::vector<Color*> colors = {};
-    //for (short i = 0; i < palettesize; i++) {
-    //    Color* c = new Color();
-    //    input.read(reinterpret_cast<char*>(c), sizeof(Color));
-    //}
-    //std::streampos currentPos = input.tellg();
-    //input.seekg(12, std::ios::cur);
-    ////width
-    //int width = 0;
-    //input.read(reinterpret_cast<char*>(&width), sizeof(width));
-    ////height
-    //int height = 0;
-    //input.read(reinterpret_cast<char*>(&height), sizeof(height));  
-    //BITMAPINFO bmi;
-    //ZeroMemory(&bmi, sizeof(BITMAPINFO));
-    //bmi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
-    //bmi.bmiHeader.biWidth = width;
-    //bmi.bmiHeader.biHeight = -height;
-    //bmi.bmiHeader.biPlanes = 1;
-    //bmi.bmiHeader.biBitCount = 24;
-    //bmi.bmiHeader.biCompression = BI_RGB;
-    //HDC hdc = GetDC(NULL);
-    //void* pBits = NULL;
-    //HBITMAP hBitmap = CreateDIBSection(hdc, &bmi, DIB_RGB_COLORS, &pBits, NULL, 0);
-    //if (hBitmap && pBits) {
-    //    int rowBytes = ((width * 3 + 3) & ~3);
-    //    BYTE* pRow = (BYTE*)pBits;
-    //    for (int y = 0; y < height; ++y) {
-    //        for (int x = 0; x < width; ++x) {
-    //            byte data = 0;
-    //            input.read(reinterpret_cast<char*>(&data), sizeof(data));
-    //            Color* color = colors[data];
-    //            pRow[x * 3 + 0] = color->B;
-    //            pRow[x * 3 + 1] = color->G;
-    //            pRow[x * 3 + 2] = color->R;
-    //        }
-    //        pRow += rowBytes;
-    //    }
-    //}
-    //ReleaseDC(NULL, hdc);
-    //*phbmp = hBitmap;
-    //*pdwAlpha = WTS_ALPHATYPE::WTSAT_RGB;
-    //for (auto iter = colors.begin(); iter != colors.end(); iter++) {
-    //    delete* iter;
-    //}
-    //colors.clear();
+    *pdwAlpha = WTS_ALPHATYPE::WTSAT_RGB;
+    for (auto iter = colors.begin(); iter != colors.end(); iter++) {
+        delete* iter;
+    }
+    colors.clear();
     return S_OK;
 }
 
